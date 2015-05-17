8623f716c5bd36c765fa4a456544681a138f44e0
Your Name <you@example.com>
2015-05-17 18:57:06 +0000

    removed --rm

--- libvirtd/test.sh
+++ libvirtd/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker rm -f libvirtd
-docker run --name libvirt --rm --privileged --net=host -ti -d -e 'container=docker' \
+docker run --name libvirt --privileged --net=host -ti -d -e 'container=docker' \
 -v /proc/modules:/proc/modules -v /mnt/usb0/libvirt/:/var/lib/libvirt/ \
 -v /sys/fs/cgroup:/sys/fs/cgroup:rw libvirtd
